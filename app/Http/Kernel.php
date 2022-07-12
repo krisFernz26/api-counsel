71491444ce3cea225c475a1d15dc9a4ac43765f3
Kristoffer Kyle Fernandez <kfernz2626@gmail.com>
2022-07-12 17:47:55 +0800

    Removed customCORShandler middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Fruitcake\Cors\HandleCors::class,
-        \App\Http\Middleware\CustomCORSHandler::class
+        // \App\Http\Middleware\CustomCORSHandler::class
     ];
 
     /**
